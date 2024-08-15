a750d78fda5b7670f4671b938e11f317ada40134
larineoy <larineoyhan@gmail.com>
2024-08-14 20:37:56 -0400

    updates

--- load.py
+++ load.py
@@ -16,3 +16,16 @@ import warnings
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+base_path = os.path.dirname(os.path.abspath(__file__))  # Path of the current script
+data_path = os.path.join(base_path, 'Data')
+
+TESS = os.path.join(data_path, 'TESS')
+RAV = os.path.join(data_path, 'RAVDESS')
+SAVEE = os.path.join(data_path, 'SAVEE')
+CREMA = os.path.join(data_path, 'CREMA-D')
+
+# Process SAVEE dataset
+dir_list = os.listdir(SAVEE)
+emotion = []
+path = []
