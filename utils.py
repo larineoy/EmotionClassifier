27c21c7e93a4b933853c6dc6a7787b6127d93426
larineoy <larineoyhan@gmail.com>
2024-08-17 00:34:30 -0400

    updates

---
+++ utils.py
@@ -0,0 +1,36 @@
+import keras
+from keras import regularizers
+from keras.preprocessing import sequence
+from tensorflow.keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential, Model, model_from_json
+from keras.layers import Dense, Embedding, LSTM
+from keras.layers import Input, Flatten, Dropout, Activation, BatchNormalization
+from keras.layers import Conv1D, MaxPooling1D, AveragePooling1D
+from keras.utils import to_categorical
+from keras.callbacks import ModelCheckpoint
+
+# sklearn
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.decomposition import PCA
+
+# Other
+import librosa
+import librosa.display
+import json
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from matplotlib.pyplot import specgram
+import pandas as pd
+import seaborn as sns
+import glob
+import os
+import pickle
+import IPython.display as ipd  # To play sound in the notebook
+import numpy as np
+
+import sounddevice as sd
+from scipy.io.wavfile import write
