1b8464f78cb17b4685de55c7ef2a83996928ee79
larineoy <larineoyhan@gmail.com>
2024-08-16 23:54:13 -0400

    updates

--- model.py
+++ model.py
@@ -30,3 +30,47 @@ import os
 import pickle
 import IPython.display as ipd  # To play sound in the notebook
 import numpy as np
+
+# Load the meta-data file
+ref = pd.read_csv("./Data_path.csv") # might need revision for location
+ref.head()
+
+# Initialize DataFrame for features
+df = pd.DataFrame(columns=['feature'])
+
+# Loop feature extration over the dataset
+for index, path in enumerate(ref.path):
+    X, sample_rate = librosa.load(path, res_type='kaiser_fast', duration=2.5, sr=44100, offset=0.5)
+    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=13), axis=0)
+    df.loc[index] = [mfccs]
+
+# Combine features with meta-data
+df = pd.concat([ref, pd.DataFrame(df['feature'].values.tolist())], axis=1)
+df = df.fillna(0)
+
+# Split the data into training and testing sets
+X = df.drop(['path', 'labels', 'source'], axis=1)
+y = df.labels
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, shuffle=True, random_state=42)
+
+# Normalize the data
+mean = np.mean(X_train, axis=0)
+std = np.std(X_train, axis=0)
+X_train = (X_train - mean) / std
+X_test = (X_test - mean) / std
+
+# Covert data format to numpy arrays and one-hot encode the target
+X_train = np.array(X_train)
+X_test = np.array(X_test)
+lb = LabelEncoder()
+y_train = to_categorical(lb.fit_transform(y_train))
+y_test = to_categorical(lb.fit_transform(y_test))
+
+# Pickle the label encoder for future use
+with open('labels', 'wb') as outfile:
+    pickle.dump(lb, outfile)
+
+# Expand the dimensions for CNN input
+X_train = np.expand_dims(X_train, axis=2)
+X_test = np.expand_dims(X_test, axis=2)
+
