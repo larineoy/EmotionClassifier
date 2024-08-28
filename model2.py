b4fff6afaf2e8c6d4e69ba8513d4512162d6b87b
larineoy <larineoyhan@gmail.com>
2024-08-27 23:54:55 -0400

    updates

--- model2.py
+++ model2.py
@@ -147,6 +147,3 @@ plt.xlabel('Predicted')
 plt.ylabel('True')
 plt.title('Confusion Matrix')
 plt.show()
-
-# Classification report
-print(classification_report(y_true, y_pred_classes, target_names=lb.classes_))
